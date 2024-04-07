c79c1aadd3d1f2937d5fc9b22fc4f737e6c0445a
rayssaw28 <161396496+rayssaw28@users.noreply.github.com>
2024-04-07 17:16:29 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,12 @@
+const botaoMostrarProjetos = document.querySelector('.btn-mostrar-projetos');
+const projetosInativos = document.querySelectorAll('.projeto:not(.ativo)');
+
+botaoMostrarProjetos.addEventListener('click', () => {
+    mostrarMaisProjetos();
+    botaoMostrarProjetos.classList.add('remover');
+})
+function mostrarMaisProjetos() {
+    projetosInativos.forEach(projetoInativo => {
+        projetoInativo.classList.add('ativo');
+    });
+}
